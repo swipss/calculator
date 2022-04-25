1505591dbb200f84b0dfdab6db653335ea37414f
Sten V <vassiljevsten1@gmail.com>
2022-04-25 11:06:13 +0300

    Calculate sum

--- script.js
+++ script.js
@@ -1,26 +1,61 @@
 
-const KEYS = ['C', '+/-', '%', '<', '7', '8', '9', '*', '4', '5', '6', '-', '1', '2',
-'3', '+', '0', '.', '/', '=']
+const KEYS = ['C', '+/-', '%', '<', 7, 8, 9, '*', 4, 5, 6, '-', 1, 2,
+3, '+', 0, '.', '/', '=']
 
 const buttonContainer = document.querySelector('.buttons-container')
 
-KEYS.forEach(key => {
-    const button = document.createElement('div')
-    button.classList.add('button')
-    button.textContent = key
-    buttonContainer.append(button)
+let displayValue = ''
+let lastValue = ''
+let operator = ''
 
-    button.addEventListener('click', (e) => {
-        const numberContainer = document.querySelector('.number-container')
-        numberContainer.textContent += key
 
-        if (key === 'C') {
-            numberContainer.textContent = ''
-        }
-        
+function populate() {
+    KEYS.forEach(key => {
+        const button = document.createElement('div')
+        button.classList.add('button')
+        button.textContent = key
+        buttonContainer.append(button)
+
+    
+        button.addEventListener('click', (e) => {
+            const numberContainer = document.querySelector('.number-container')
+            if (key === Number(key)) {
+                numberContainer.textContent = displayValue
+
+                numberContainer.textContent += key
+                displayValue += key
+            }
+            if (key === '+') {
+                numberContainer.textContent = displayValue
+                operator = '+'
+                lastValue = displayValue
+                displayValue = ''
+                console.log(lastValue)
+                console.log('plus clicked')
+
+            }
+
+            if (key === '=') {
+                let answer = operate(operator, Number(lastValue), Number(displayValue))
+                console.log(answer)
+                numberContainer.textContent = answer
+                displayValue = answer
+                operator = ''
+            }
+
+            if (key === 'C') {
+                numberContainer.textContent = ''
+                displayValue = ''
+                lastValue = ''
+                operator = ''
+            }
+            
+
+        })
+    
     })
+}
 
-})
 
 function add(num1, num2) {
     return num1 + num2
@@ -50,3 +85,5 @@ function operate(operator, num1, num2) {
         return divide(num1, num2)
     }
 }
+
+populate()
